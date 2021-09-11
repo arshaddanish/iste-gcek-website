aae2542144a4bce2287a1d77e0c6a226ed91a5ed
Arshad Danish <arshaddanishthana@gmail.com>
2021-09-11 12:57:36 +0530

    aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,4 +238,13 @@
       }
     }
   });
+
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
 })();
